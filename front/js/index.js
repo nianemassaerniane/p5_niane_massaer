4533fc547c3f25389214b09f274696c2c2a2617d
nianemassaerniane <nianemassaerniane@gmail.com>
2022-01-28 17:33:58 +0100

    lancement de l'API recuperation des donnee de l'api

--- front/js/index.js
+++ front/js/index.js
@@ -1,1 +1,7 @@
-console.log("salut");
+fillSection();
+
+// Lancement et Récupération des données de l'API
+async function kanapDonnee() {
+    var recupereArticle = await fetch("http://localhost:3000/api/products")
+    return await recupereArticle.json();
+}
