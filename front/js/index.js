9f28e01b6192771cc98e661c3850f69052c6f9ca
nianemassaerniane <nianemassaerniane@gmail.com>
2022-01-28 18:05:43 +0100

    Integration et répartition des donée dans la page d'accueil

--- front/js/index.js
+++ front/js/index.js
@@ -5,3 +5,16 @@ async function kanapDonnee() {
     var recupereArticle = await fetch("http://localhost:3000/api/products")
     return await recupereArticle.json();
 }
+// INTEGRATION  ET REPARTITION  DES DONNEES DANS LA PAGE D'ACCUEIL
+
+async function sectionRemplir() {
+    var result = await kanapDonnee ()
+    .then(function (resultatAPI){
+        const articles = resultatAPI;
+        console.table(articles);
+        for (let article in articles) {
+        
+        }
+    })
+}
+
