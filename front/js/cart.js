73a19b6d3eb6b58d8c12cbb03af01e706269b9e7
nianemassaerniane <nianemassaerniane@gmail.com>
2022-01-29 14:57:11 +0100

    parametrage de la gestion du tablaux cas ou il n'y aurait pas d'article

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,4 +2,13 @@
 let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
 console.table(produitLocalStorage);
 const placerPanierVide = document.querySelector("#cart__items");
+// Si le panier est vide
+function afficherPanier(){
 
+    if (produitLocalStorage === null || produitLocalStorage == 0) {
+        const panierVide = `<p>Votre panier est vide</p>`;
+        placerPanierVide.innerHTML = panierVide;
+    } 
+}
+
+    afficherPanier();
